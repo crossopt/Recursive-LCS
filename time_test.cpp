13585e4fc8f9dd0809f5b271d51b385616b5e72a
crossopt <s17b2_fedorkina@179.ru>
2020-05-13 00:03:49 +0300

    add timing

--- time_test.cpp
+++ time_test.cpp
@@ -37,6 +37,15 @@ double time_recursive(const std::string &a, const std::string &b) {
     return diff.count();
 }
 
+double time_recursive_base(const std::string &a, const std::string &b, unsigned recursion_base) {
+    time_point<Clock> start = Clock::now();
+    auto kernel = LCS::kernel::RecursiveLCS(a, b, recursion_base);
+    kernel.lcs_whole_a(0, b.size());
+    time_point<Clock> end = Clock::now();
+    milliseconds diff = duration_cast<milliseconds>(end - start);
+    return diff.count();
+}
+
 double time_iterative(const std::string &a, const std::string &b) {
     time_point<Clock> start = Clock::now();
     auto kernel = LCS::kernel::IterativeLCS(a, b);
@@ -53,7 +62,18 @@ void test_strings(const std::string &a, const std::string &b) {
     std::cout << "Time for recursive kernel is " << time_recursive(a, b) << "ms" << std::endl;
 }
 
+void test_recursive_bases(const std::string &a, const std::string &b) {
+    std::cout << "Times for Recursive LCS calculation for string lengths " << a.size() << " and " << b.size() << ":" << std::endl;
+    std::cout << "Time for base 4 is " << time_recursive_base(a, b, 4) << "ms" << std::endl;
+    std::cout << "Time for base 8 is " << time_recursive_base(a, b, 8) << "ms" << std::endl;
+    std::cout << "Time for base 16 is " << time_recursive_base(a, b, 16) << "ms" << std::endl;
+    std::cout << "Time for base 32 is " << time_recursive_base(a, b, 32) << "ms" << std::endl;
+}
+
 int main() {
+    test_recursive_bases(generate_random_string(100), generate_random_string(200));
+    test_recursive_bases(generate_random_string(500), generate_random_string(500));
+
     test_strings(generate_random_string(100), generate_random_string(200));
     test_strings(generate_random_string(500), generate_random_string(500));
     test_strings(generate_random_string(1000), generate_random_string(1000));
