ff331b3de3dd550fbcbb59c5ad25ab81b671415d
Ivan Shershnev <ivankrut856@yandex.ru>
2021-05-31 13:54:24 +0300

    run

--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for (( c=200000; c<=1000000000; c += 200000 ))
+for (( c=11000000; c<=1000000000; c += 500000 ))
 do
 	# for cycle is too long, break when enough
 	time_lz=0
 	time_dp=0
 	time_agrep=0
 	lenf=0
-	for (( i=0; i<=10; i++))
+	for (( i=0; i<=4; i++))
 	do
 
 	    # Generate pattern
@@ -16,16 +16,17 @@ do
 	    printf 'a%.0s' $(seq 1 $c) > file
 	    # Generation: fake texts
 	    # echo $c $i | python3 zgen.py
+	    # lena=$(cat file | wc -c)
 	    # Generation: real texts
 	    # st=$((($i * $c)))
-	    # cat test | tail -c +$st | head -c $c > file
+	    # cat test_files/rand_file | tail -c +$st | head -c $c > file
 	    compress -f file
 	    lenf=$(cat file.Z | wc -c)
 
 	    
-	    ts=$(date +%s%N) ; printf $pattern | ./build/dp_test; tt=$((($(date +%s%N) - $ts))) ; time_dp=$((time_dp + tt)); # echo dp $tt
-	    ts=$(date +%s%N) ; echo $c $pattern | ./build/lz_test; tt=$((($(date +%s%N) - $ts))) ; time_lz=$((time_lz + tt)); # echo lz $tt
-	    ts=$(date +%s%N) ; printf $pattern | ./run_agrep.sh; tt=$((($(date +%s%N) - $ts))) ; time_agrep=$((time_agrep + tt)); # echo agrep $tt
+	    ts=$(date +%s%N) ; printf $pattern | ./build/dp_test 2> /dev/null | cat; tt=$((($(date +%s%N) - $ts))) ; time_dp=$((time_dp + tt)); # echo dp $tt
+	    ts=$(date +%s%N) ; echo $c $i $pattern | ./build/lz_test; tt=$((($(date +%s%N) - $ts))) ; time_lz=$((time_lz + tt)); # echo lz $tt
+	    ts=$(date +%s%N) ; printf $pattern | ./run_agrep.sh 2> /dev/null | cat; tt=$((($(date +%s%N) - $ts))) ; time_agrep=$((time_agrep + tt)); # echo agrep $tt
 	done
 	# / 20 / 1000000
 	echo $c $lenf $time_agrep $time_lz $time_dp
